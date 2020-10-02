50f33cce0ee85432d5c25a23f66ddf7221a0ac6e
Gaston Guillaux <ggx.dev@outlook.com>
2020-10-01 22:23:44 -0300

    script for docker automation added

---
+++ install_docker.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# update ubuntu
+sudo apt update
+sudo apt upgrade
+
+# install docker
+sudo apt install docker.io
+sudo systemctl start docker
+sudo systemctl enable docker
+
+# update ubuntu again
+sudo apt update
+sudo apt upgrade
+
+# check docker version and status
+sudo systemctl status docker
+echo " ===================\n\n ===================\n"
+docker -v
